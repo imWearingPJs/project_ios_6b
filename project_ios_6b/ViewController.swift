fcfa3a4e95864243237b579295b8c81578ed8ce5
Michael Kozub <michaelkozub@gmail.com>
2018-11-13 17:10:58 -0700

    first commit

---
+++ project_ios_6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project_ios_6b
+//
+//  Created by Michael Kozub on 11/13/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
